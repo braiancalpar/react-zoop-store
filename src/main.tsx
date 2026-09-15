3aba4444ac77635ddf3f3069caf503a43bec7c4b
braiancalpar <braian.santos@calpar.com.br>
2026-09-15 15:02:52 -0300

    adiciona monitoramento de métricas vitais com web-vitals

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,11 @@ import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import App from './App.tsx';
+import vitalsMonitor from './services/vitals.ts';
+
+vitalsMonitor.init({
+  enableConsoleLog: import.meta.env.DEV,
+});
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
